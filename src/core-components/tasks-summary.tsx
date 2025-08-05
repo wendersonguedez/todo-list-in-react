eea9de89c684a7c7c273f869fe6db7eebab46524
wenderson <wendersonguedes6@gmail.com>
2025-08-05 00:02:01 -0300

    feat: implementando dinamismo aos badges

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,20 +1,25 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksSummary() {
+	const { tasksCount, concludedTasksCount } = useTasks();
+
 	return (
 		<>
 			<div className="flex items-center gap-2">
 				<Text variant={"body-sm-bold"} className="!text-gray-300">
 					Tarefas criadas
 				</Text>
-				<Badge variant={"secondary"}>5</Badge>
+				<Badge variant={"secondary"}>{tasksCount}</Badge>
 			</div>
 			<div className="flex items-center gap-2">
 				<Text variant={"body-sm-bold"} className="!text-gray-300">
 					Concluídas
 				</Text>
-				<Badge variant={"primary"}>2 de 5</Badge>
+				<Badge variant={"primary"}>
+					{concludedTasksCount} de {tasksCount}
+				</Badge>
 			</div>
 		</>
 	);
--- src/hooks/use-tasks.ts
+++ src/hooks/use-tasks.ts
@@ -1,12 +1,12 @@
 import useLocalStorage from "use-local-storage";
-import { TASK_KEY, type Task } from "../models/task";
+import { TASK_KEY, TaskState, type Task } from "../models/task";
 
 export default function useTasks() {
 	const [tasks] = useLocalStorage<Task[]>(TASK_KEY, []);
 
 	return {
 		tasks,
-		tasksCount: tasks.length,
+		tasksCount: tasks.filter((task) => task.state === TaskState.Created).length,
 		concludedTasksCount: tasks.filter((task) => task.concluded).length,
 	};
 }
